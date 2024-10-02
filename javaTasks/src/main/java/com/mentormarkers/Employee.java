aef5d20dd2a5d761487e8f15756be972834a33d1
martindamian13 <cachobnitz@icloud.com>
2024-10-02 15:14:39 -0400

    reworked with getters and setters

--- javaTasks/src/main/java/com/mentormarkers/Employee.java
+++ javaTasks/src/main/java/com/mentormarkers/Employee.java
@@ -9,31 +9,69 @@ public class Employee {
 
     // Constructor
     public Employee(String firstName, String lastName, int age, int employeeNumber) {
-        if (age < 0 || age > 100) {
-            System.out.println("Age must be between 0 and 100");
-        }
-        if (employeeNumber < 0 || employeeNumber > 9999) {
-            System.out.println("Employee number must be between 0 and 9999");
-        }
+        setFirstName(firstName);
+        setLastName(lastName);
+        setAge(age);
+        setEmployeeNumber(employeeNumber);
+        setEmail(firstName, lastName);
+    }
+
+    // Getters and setters
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
         this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
         this.age = age;
+
+    }
+
+    public int getEmployeeNumber() {
+        return employeeNumber;
+    }
+
+    public void setEmployeeNumber(int employeeNumber) {
         this.employeeNumber = employeeNumber + 27560000;
-        this.email = generateemail(firstName, lastName);
+
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String firstName, String lastName) {
+        this.email = generateEmail(firstName, lastName);
     }
 
     // Method to generate email address
-    public String generateemail(String firstName, String lastName) {
+    public String generateEmail(String firstName, String lastName) {
         return firstName.toLowerCase() + "." + lastName.toLowerCase() + "@mentormakers.com";
     }
 
     // Method to display employee information
     public void displayEmployeeInfo() {
+        // Use getters to access private fields
         System.out.println("Employee Information:");
-        System.out.println("First Name: " + firstName);
-        System.out.println("Last Name: " + lastName);
-        System.out.println("Age: " + age);
-        System.out.println("Employee Number: " + employeeNumber);
-        System.out.println("Email: " + email);
+        System.out.println("First Name: " + getFirstName());
+        System.out.println("Last Name: " + getLastName());
+        System.out.println("Age: " + getAge());
+        System.out.println("Employee Number: " + getEmployeeNumber());
+        System.out.println("Email: " + getEmail());  // Email is already generated
     }
 }
